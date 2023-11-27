d58853327625f7f4691d4a63f73f7d9162aa2770
Arthur Willian Assunção Francisco <109974170+ArthurWillianAFrancisco@users.noreply.github.com>
2023-11-27 18:25:27 -0400

    Update cliente-service.js

--- passeio/projeto_final/admin/service/cliente-service.js
+++ passeio/projeto_final/admin/service/cliente-service.js
@@ -1,86 +1,72 @@
-const baseUrl = 'https://my-json-server.typicode.com/ArthurWillianAFrancisco/Meu-passeio/profile';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const listaClientes = () =>  {
-    return fetch(`${baseUrl}`)
-    .then(resposta => {
-        if(resposta.ok){
-            return resposta.json()
-        }
-        throw new Error('Não foi possível listar os clientes')
-    })
-}
+const app = express();
+const PORT = 3000;
 
-const criaCliente = (nome, nome_lugar, local_pais, cidade, descricao) => { 
-    return fetch(`${baseUrl}`, {
-        method: 'POST', 
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify({
-            nome: nome,
-            nome_lugar: nome_lugar,
-            local_pais: local_pais,
-            cidade: cidade,
-            descricao: descricao,
-        })
-    })
-    .then( resposta => {
-        if(resposta.ok){
-            return resposta.body
-        }
-        throw new Error('Não foi possível criar um cliente')
-    })
-}
+app.use(bodyParser.json());
+app.use(cors());
 
-const removeCliente = (id) => { 
-    return fetch(`${baseUrl}/${id}`, {
-        method: 'DELETE'
-    })
-    .then( resposta => { 
-        if(!resposta.ok){
-        throw new Error('Não foi possível deletar um cliente')
-        }
-    })
-}
- 
-const detalhaCliente = (id) => { 
-    return fetch(`${baseUrl}/${id}`)
-    .then(resposta => { 
-        if(resposta.ok){
-            return resposta.json()
-        }
-    
-        throw new Error('Não foi possível detalhar um cliente')
-    })
-}
+mongoose.connect('mongodb://localhost:27017/seu-banco-de-dados', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const atualizaCliente = (id, nome, nome_lugar, local_pais, cidade, descricao) => {
-    return fetch(`${baseUrl}/${id}`, {
-        method: 'PUT',
-        headers: { 
-            'Content-type' : 'application/json'
-        },
-        body: JSON.stringify({
-            nome: nome,
-            nome_lugar: nome_lugar,
-            local_pais: local_pais,
-            cidade: cidade,
-            descricao: descricao,
+const clienteSchema = new mongoose.Schema({
+  nome: String,
+  nome_lugar: String,
+  local_pais: String,
+  cidade: String,
+  descricao: String,
+});
 
-        })
-    })
-    .then( resposta => {
-        if(resposta.ok){
-            return resposta.json()
-        }
-        throw new Error('Não foi possível detalhar um cliente')
-    })
-}
+const Cliente = mongoose.model('Cliente', clienteSchema);
 
-export const clienteService = { 
-    listaClientes,
-    criaCliente, 
-    removeCliente,
-    detalhaCliente,
-    atualizaCliente
-}
+app.get('/clientes', async (req, res) => {
+  try {
+    const clientes = await Cliente.find();
+    res.json(clientes);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.post('/clientes', async (req, res) => {
+  try {
+    const novoCliente = new Cliente(req.body);
+    await novoCliente.save();
+    res.status(201).send('Cliente criado com sucesso!');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete('/clientes/:id', async (req, res) => {
+  try {
+    await Cliente.findByIdAndDelete(req.params.id);
+    res.send('Cliente deletado com sucesso!');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get('/clientes/:id', async (req, res) => {
+  try {
+    const cliente = await Cliente.findById(req.params.id);
+    res.json(cliente);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.put('/clientes/:id', async (req, res) => {
+  try {
+    const clienteAtualizado = await Cliente.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(clienteAtualizado);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
